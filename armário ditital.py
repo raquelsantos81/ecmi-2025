0f5c6539520c6e0bbd3daa64ec4edecf1a359313
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 19:57:39 -0300

    Update armário ditital.py

--- armário ditital.py
+++ armário ditital.py
@@ -1,19 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""
-# Seu armário digital 
+st.write("# Seu armário digital\nHey, *cutie!*\nVamos escolher o outfit de hoje!") 
 
-Hey, *cute*
-vamos escolher o outfit de hoje!
-""")
 st.image("https://github.com/raquelsantos81/ecmi-armario-digital/blob/main/barbie_armario.png?raw=true", caption="Barbie Armário")
 
 ocasiao = st.selectbox(
     'Escolha sua ocasião:',
-    ['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho']
+    ['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho', 'Cinema', 'Date', 'Brunch']
 )
 
-if ocasião:
-    st.write(f"Vamos montar um look para: **{ocasião}**")
+if ocasiao:
+    st.write(f"Vamos montar um look para: **{ocasiao}**")
 
