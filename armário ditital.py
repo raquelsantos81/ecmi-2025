e33d2edc4d309d0ccfc6e14fa92ccd369cab834c
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 19:35:20 -0300

    Update armário ditital.py

--- armário ditital.py
+++ armário ditital.py
@@ -4,4 +4,15 @@ import pandas as pd
 st.write("""
 # Seu armário digital 
 Hey, *cute*
+vamos escolher o outfit de hoje!
 """)
+![imagem barbie armario](https://github.com/raquelsantos81/ecmi-2025/blob/main/HJL66---Playset-Barbie-com-Boneca---O-Closet-Perfeito-1.webp?raw+true)
+
+<label for="cidade">Escolha sua ocasião:</label>
+<select id="ocasião" name="ocasião">
+<option value="">--Selecione uma opção--</option>
+<option value="almoço">Almoço</option>
+<option value="passeio_de_dia">Passeio de dia</option>
+<option value="balada">Balada</option>
+<option value="barzinho">Barzinho</option>
+</select>
