2c1e6155b40baf8e5283fa1bfa976af8d129a1eb
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 19:47:22 -0300

    Update armário ditital.py

--- armário ditital.py
+++ armário ditital.py
@@ -10,10 +10,10 @@ vamos escolher o outfit de hoje!
 st.image("https://github.com/raquelsantos81/ecmi-armario-digital/blob/main/barbie_armario.png?raw=true", caption="Barbie Armário")
 
 ocasiao = st.selectbox(
-'Escolha sua ocasião:',
-['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho']
+    'Escolha sua ocasião:',
+    ['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho']
 )
 
-if ocasiao:
-st.write(f"Vamos montar um look para: **{ocasiao}**")
+if ocasião:
+    st.write(f"Vamos montar um look para: **{ocasião}**")
 
