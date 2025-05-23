5a3b09f2ad0b0b106320909e295232bd3b9348e7
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 20:12:27 -0300

    Update armário digital.py

--- armário digital.py
+++ armário digital.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("# Seu armário digital\nHey, *cutie!*\nVamos escolher o outfit de hoje!") 
-st.image("https://github.com/raquelsantos81/ecmi-2025/blob/main/HJL66---Playset-Barbie-com-Boneca---O-Closet-Perfeito-1.webp?raw=true", caption="Barbie Armário")
+st.image("https://github.com/raquelsantos81/ecmi-2025/blob/main/HJL66---Playset-Barbie-com-Boneca---O-Closet-Perfeito-1.webp?raw=true")
 
 ocasiao = st.selectbox(
     'Escolha sua ocasião:',
