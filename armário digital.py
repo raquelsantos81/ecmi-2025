9f74abb3ab5344f9feab2772d50b490cf89aeeeb
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 20:09:04 -0300

    Update armário digital.py

--- armário digital.py
+++ armário digital.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.write("# Seu armário digital\nHey, *cutie!*\nVamos escolher o outfit de hoje!") 
-
 st.image("https://github.com/raquelsantos81/ecmi-2025/blob/main/HJL66---Playset-Barbie-com-Boneca---O-Closet-Perfeito-1.webp?raw=true", caption="Barbie Armário")
 
 ocasiao = st.selectbox(
     'Escolha sua ocasião:',
-    ['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho', 'Cinema', 'Date', 'Brunch']
+    ['', 'Almoço', 'Passeio de dia', 'Balada', 'Barzinho', 'Cinema', 'Date', 'Brunch', 'Churrasco', 'Academia', 'Museu']
 )
 
 if ocasiao:
