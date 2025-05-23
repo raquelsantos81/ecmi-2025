cc82a431d952202b98c26d5359ba65dc6aa002cb
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-23 19:59:40 -0300

    Update armário ditital.py

--- armário ditital.py
+++ armário ditital.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.write("# Seu armário digital\nHey, *cutie!*\nVamos escolher o outfit de hoje!") 
 
-st.image("https://github.com/raquelsantos81/ecmi-armario-digital/blob/main/barbie_armario.png?raw=true", caption="Barbie Armário")
+st.image("https://github.com/raquelsantos81/ecmi-2025/blob/main/HJL66---Playset-Barbie-com-Boneca---O-Closet-Perfeito-1.webp?raw=true", caption="Barbie Armário")
 
 ocasiao = st.selectbox(
     'Escolha sua ocasião:',
